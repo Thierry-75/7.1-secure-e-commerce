86a75f3fa216155dafbd05b5ffdbb0fac6d4b040
Enki <104570540+Thierry-75@users.noreply.github.com>
2024-10-14 19:12:45 +0200

    implement method register & form -civility-

--- src/Controller/CivilityController.php
+++ src/Controller/CivilityController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Civility;
+use App\Form\CivilityFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CivilityController extends AbstractController
 {
-    #[Route('/civility', name: 'app_civility')]
-    public function index(): Response
+    #[Route('/civility/register', name: 'app_civility_register', methods: ['GET', 'POST'])]
+    public function register(Request $request, EntityManagerInterface $em,  ValidatorInterface $validator): Response
     {
-        return $this->render('civility/index.html.twig', [
-            'controller_name' => 'CivilityController',
-        ]);
+        $user = $this->getUser();
+        //dd($user);
+        if (!$user) {
+            $this->addFlash('alert-danger', 'Vous devez être connecté pour accéder à cette page !');
+            return $this->redirectToRoute('app_login');
+        }
+        if (!$this->getUser()->isCompleted() == false) {
+            $this->addFlash('alert-warning', 'Ce compte est déjà activé !');
+            return $this->redirectToRoute('app_main');  // redirect to profil
+        }
+        $civility = new Civility();
+        $form_register = $this->createForm(CivilityFormType::class, $civility);
+        $form_register->handleRequest($request);
+        if ($request->isMethod('POST')) {
+            $errors = $validator->validate($civility);
+            if (count($errors) > 0) {
+                return $this->render('/civility/register.html.twig', ['form_register' => $form_register->createView(), 'errors' => $errors]);
+            }
+            if ($form_register->isSubmitted() && $form_register->isValid()) {
+                $civility->setClient($this->getUser());
+                $em->persist($civility);
+                $em->flush();
+                $this->addFlash('alert-success', 'Vos coordonnées ont été enregistrées !');
+                return $this->redirectToRoute('app_main'); // vers profile ?
+            }
+        }
+        return $this->render('/civility/register.html.twig', ['form_register' => $form_register->createView()]);
     }
 }
--- src/Form/CivilityFormType.php
+++ src/Form/CivilityFormType.php
@@ -2,31 +2,86 @@
 
 namespace App\Form;
 
-use App\Entity\Civility;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Civility;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use function PHPUnit\Framework\returnSelf;
 
 class CivilityFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class)
-            ->add('prenom',TextType::class)
-            ->add('telephone',TelType::class)
-            ->add('numero',NumberType::class)
-            ->add('adresse',TextType::class)
-            ->add('codePostal',NumberType::class)
-            ->add('ville',TextType::class)
-            ->add('submit',SubmitType::class)
+            ->add('nom',TextType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                'label'=>'Nom :',
+                'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('prenom',TextType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Prénom :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('telephone',TelType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Téléphone :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('numero',NumberType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Numéro de rue :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('adresse',TextType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Adresse :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],])
+            ->add('codePostal',NumberType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Code postal :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('ville',TextType::class,[
+                'attr' => ['class' => 'rounded-lg bg-gray-50 border border-gray-300 text-gray-900 text-xs 
+                  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder' => ''],
+                   'label'=>'Ville :',
+                   'label_attr'=>['class'=>'block mb-1 text-xs font-light text-gray-500 dark:text-gray-400'],
+                   ])
+            ->add('submit',SubmitType::class,['attr'=>['class'=>'w-full btn-info my-0.5'],
+            'label'=>'Valider'
+            ])
+            ->addEventListener(FormEvents::POST_SUBMIT,$this->addDate(...))
         ;
+    }
+
+    public function addDate(PostSubmitEvent $event)
+    {
+        $data = $event->getData();
+        if(!($data instanceof Civility)) return;
+        $data->setCreatedAt(new \DateTimeImmutable());
     }
 
     public function configureOptions(OptionsResolver $resolver): void
